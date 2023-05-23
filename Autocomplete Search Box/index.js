8af1c59c566d7ff268a1bf0422fe8c4893075e52
Mahua Chanda P <mahuachanda@gmail.com>
2023-05-23 08:04:05 +0530

    added some new features

--- Autocomplete Search Box/index.js
+++ Autocomplete Search Box/index.js
@@ -6,6 +6,9 @@ let availableKeywords = [
   "How to learn Web design",
   "How to create website",
   "How to succeed in an interview",
+  "How to learn CSS within a week",
+  "Lets crack the frontend interview",
+  "Win my heart",
 ];
 
 const resultBox = document.querySelector(".result-box");
@@ -13,25 +16,27 @@ const inputBox = document.getElementById("input-box");
 
 inputBox.onkeyup = function () {
   let result = [];
-  let input = inputBox.value;
+  const input = inputBox.value;
   if (input.length > 0) {
     result = availableKeywords.filter((keyword) => {
       return keyword.toLowerCase().includes(input.toLowerCase());
     });
-    console.log(result);
   }
   display(result);
+
+  if (!result.length) {
+    resultBox.innerHTML = "";
+  }
 };
 
 function display(result) {
   const content = result.map((item) => {
-    return `<li onclick="selected(this)">${item}</li>`;
+    return `<li onclick = "selected(this)">${item}</li>`;
   });
-
-  resultBox.innerHTML = `<ul>${content.join("")}</ul>`;
+  resultBox.innerHTML = `<ul>${content.join("")}</ul>`; //as map creates a new array-one comma will added to remove that comma in the ui, we need to add join('')
 }
 
-function selected(item) {
-  inputBox.value = item.innerHTML;
+function selected(list) {
+  inputBox.value = list.innerHTML;
   resultBox.innerHTML = "";
 }
